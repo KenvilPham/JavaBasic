8c678c756bb9d09a142359ec5e956909eeaa9c3e
KenvilPham <kenvil.pham92@gmail.com>
2021-01-26 21:34:26 +0700

    baitapvn day1

---
+++ src/com/home/javabasic/day1/Circle.java
@@ -0,0 +1,19 @@
+package com.home.javabasic.day1;
+
+import java.util.Scanner;
+
+public class Circle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    double circleRadius, circlePerimeter, circleArea;
+        System.out.println("Input circle radius: ");
+        circleRadius = sc.nextDouble();
+
+        circlePerimeter = 2 * Math.PI * circleRadius;
+        circleArea = Math.PI * circleRadius * circleRadius;
+
+        System.out.printf("\nPerimeter of circle is: %.3f", circlePerimeter);
+        System.out.printf("\nArea of circle is: %.3f", circleArea);
+    }
+}
